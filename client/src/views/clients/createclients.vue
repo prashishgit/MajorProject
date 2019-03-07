2e69e654b1396ed28640b630bfab0be536f1ad85
prashish shrestha <pralex0009@gmail.com>
2019-03-07 10:21:23 +0545

    client validation complete

--- client/src/views/clients/createclients.vue
+++ client/src/views/clients/createclients.vue
@@ -4,23 +4,23 @@
       <div slot="header" class="clearfix">
         <label>Create Clients</label>
       </div>
-      <el-form ref="form" :model="form" label-width="120px">
+      <el-form ref="form" :model="form" :rules="rules" label-width="120px">
         <el-row>
           <el-col :span="8">
             <div class="grid-content bg-purple"></div>
-            <el-form-item label="Person:">
+            <el-form-item label="Person:" prop="person">
               <el-input v-model="form.person"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="8">
             <div class="grid-content bg-purple"></div>
-            <el-form-item label="Mobile Number:">
+            <el-form-item label="Mobile Number:" prop="mobile">
               <el-input v-model="form.mobile"></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="8">
-            <el-form-item label="Currency:">
-              <el-select v-model="form.value" placeholder="Select the currency">
+            <el-form-item label="Currency:" prop="currency">
+              <el-select v-model="form.currency" placeholder="Select the currency">
                 <el-option
                   v-for="item in options"
                   :key="item.value"
@@ -34,7 +34,7 @@
         <el-row>
           <el-col :span="8">
             <div class="grid-content bg-purple"></div>
-            <el-form-item label="Company:">
+            <el-form-item label="Company:" prop="company">
               <el-input v-model="form.company"></el-input>
             </el-form-item>
           </el-col>
@@ -53,7 +53,7 @@
         <el-row>
           <el-col :span="8">
             <div class="grid-content bg-purple"></div>
-            <el-form-item label="Email:">
+            <el-form-item label="Email:" prop="email">
               <el-input v-model="form.email" placeholder="Email"></el-input>
             </el-form-item>
           </el-col>
@@ -72,7 +72,7 @@
         </el-row>
         <el-row>
           <el-col :span="8">
-            <el-form-item label="Billing Address">
+            <el-form-item label="Billing Address" prop="billing">
               <el-input
                 type="textarea"
                 :autosize="{ minRows: 3, maxRows: 6}"
@@ -82,7 +82,7 @@
             </el-form-item>
           </el-col>
           <el-col :span="8">
-            <el-form-item label="Shipping Address">
+            <el-form-item label="Shipping Address" prop="shipping">
               <el-input
                 type="textarea"
                 :autosize="{ minRows: 3, maxRows: 6}"
@@ -94,7 +94,7 @@
         </el-row>
         <el-form-item>
           <el-button type="primary" @click="onSubmit('form')">Create</el-button>
-          <el-button @click="resetForm('form')">Reset</el-button>
+          <el-button @click="onCancel('form')">Reset</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -108,6 +108,16 @@ Vue.prototype.$http = axios;
 import { addPost } from "@/api/client";
 export default {
   data() {
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("Please input all the fields"));
+      } else {
+        if (this.form.checkPass !== "") {
+          this.$refs.form.validateField("checkPass");
+        }
+        callback();
+      }
+    };
     return {
       form: {
         person: "",
@@ -141,7 +151,36 @@ export default {
           label: "Euro"
         }
       ],
-      value: ""
+      value: "",
+      rules: {
+        person: [
+          {
+            required: true,
+            message: "Please input person",
+            trigger: "blur"
+          }
+        ],
+        company: [
+          { required: true, message: "Please input company", trigger: "blur" }
+        ],
+        currency: [
+          { required: true, message: "Please input currency", trigger: "blur" }
+        ],
+        email: [
+          { required: true, message: "Please input email", pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, trigger: "blur" }
+        ],
+
+
+        mobile: [
+          { required: true, message: "Please input mobile", trigger: "blur" }
+        ],
+        billing: [
+          { required: true, message: "Please input billing", trigger: "blur" }
+        ],
+        shipping: [
+          { required: true, message: "Please input shipping", trigger: "blur" }
+        ]
+      }
     };
   },
   // created: function(){
@@ -149,36 +188,39 @@ export default {
   // },
   methods: {
     onSubmit(formName) {
-      this.loading = true;
-     
-      axios.post('https://localhost:5001/api/client', {
-       Person: this.form.person,
-        Mobile_Number: this.form.mobile,
-        Currency: this.form.value,
-        Company: this.form.company,
-        Work_Phone: this.form.work,
-        Email: this.form.email,
-        Billing_Address: this.form.billing,
-        Shipping_Address: this.form.shipping,
-        Created: new Date()
-      }).then(res => {
-        this.$router.back();
-        this.loading = false;
-        //NProgress.done();
-        this.$message({
-          type: "success",
-          message: "Client Add completed"
-        });
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.loading = true;
+
+          axios
+            .post("https://localhost:5001/api/client", {
+              Person: this.form.person,
+              Mobile_Number: this.form.mobile,
+              Currency: this.form.value,
+              Company: this.form.company,
+              Work_Phone: this.form.work,
+              Email: this.form.email,
+              Billing_Address: this.form.billing,
+              Shipping_Address: this.form.shipping,
+              Created: new Date()
+            })
+            .then(res => {
+              this.$router.back();
+              this.loading = false;
+              //NProgress.done();
+              this.$message({
+                type: "success",
+                message: "Client Add completed"
+              });
+            });
+        } else {
+          this.$message({
+            type: "info",
+            message: "client Add Canceled"
+          });
+          return false;
+        }
       });
-      // addPost(clientParams).then(res => {
-      //   this.$router.back();
-      //   this.loading = false;
-      //   //NProgress.done();
-      //   this.$message({
-      //     type: "success",
-      //     message: "Client Add completed"
-      //   });
-      // });
     }
     // resetForm(formName) {
     //   this.$refs[formName].resetFields();
